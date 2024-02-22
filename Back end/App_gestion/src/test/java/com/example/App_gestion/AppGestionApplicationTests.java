ed1da6df7ddcc346ba54601f2f7880ef5847336c
JAlejandroDiaz <129114007+JAlejandroDiaz@users.noreply.github.com>
2024-02-22 18:09:01 -0500

    back end
    b

---
+++ Back end/App_gestion/src/test/java/com/example/App_gestion/AppGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.App_gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
