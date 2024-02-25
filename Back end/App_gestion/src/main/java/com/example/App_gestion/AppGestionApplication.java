f967af422a9c910105638c39b3aaa19a33b77616
JAlejandroDiaz <160150408+AlejandroDiazVera@users.noreply.github.com>
2024-02-25 14:53:53 -0500

    prueba 1

--- Back end/App_gestion/src/main/java/com/example/App_gestion/AppGestionApplication.java
+++ Back end/App_gestion/src/main/java/com/example/App_gestion/AppGestionApplication.java
@@ -18,7 +18,7 @@ public class AppGestionApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hi %s!", name);
 	}
 
 }
