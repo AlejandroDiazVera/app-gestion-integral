9b3e055f2e730ff3785f5a513a97df528307fdad
JAlejandroDiaz <160150408+AlejandroDiazVera@users.noreply.github.com>
2024-02-29 16:07:12 -0500

    crud de negocio parmente completo

--- Back end/App_gestion/src/main/java/com/example/App_gestion/Controllers/controllernegocio.java
+++ Back end/App_gestion/src/main/java/com/example/App_gestion/Controllers/controllernegocio.java
@@ -1,7 +1,7 @@
 package com.example.App_gestion.Controllers;
 
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +12,33 @@ import com.example.App_gestion.Services.negocioservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
-@RequestMapping(path ="/api/v1/negocio")
+@RequestMapping(path = "/api/v1/negocio")
 public class controllernegocio {
-    
+
     @Autowired
     private negocioservice negocioservice;
-    
+
     @GetMapping
-    public List<negocio> getAll(){
-        return  negocioservice.getNegocio();
+    public List<negocio> getAll() {
+        return negocioservice.getAllNegocio();
 
     }
+    
+    @GetMapping ("{Idnegocio}")
+    public Optional<negocio> getByIdAll(@PathVariable("Idnegocio") Long Idnegocio) {
+        return negocioservice.getByIdNegocio(Idnegocio);
 
+    }
+   
+   @DeleteMapping("{Idnegocio}")
+   public void deletebyId(@PathVariable("Idnegocio") Long Idnegocio){
+    negocioservice.deleteByid(Idnegocio);
+   }
     
-    
+   @PostMapping
+   public void saveOrUpdate(@RequestBody negocio mynegocio){
+      negocioservice.saveOrUpdate(mynegocio);
+   }
+
 }
--- Back end/App_gestion/src/main/java/com/example/App_gestion/Services/negocioservice.java
+++ Back end/App_gestion/src/main/java/com/example/App_gestion/Services/negocioservice.java
@@ -1,6 +1,7 @@
 package com.example.App_gestion.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,33 @@ import com.example.App_gestion.repository.negociorepository;
 @Service
 // @Transactional
 public class negocioservice {
-   
+
     @Autowired
     negociorepository negociorepository;
 
-
-    public List<negocio> getNegocio(){
+    public List<negocio> getAllNegocio() {
         return negociorepository.findAll();
     }
+
+    public Optional<negocio> getByIdNegocio(Long id){
+        return negociorepository.findById(id);
+    }
+
+
+    public void deleteByid(Long id){
+    
+        negociorepository.deleteById(id);
+        
+        
+    }
+
+
+    public void saveOrUpdate(negocio mynegocio){
+        negociorepository.save(mynegocio);
+    }
+
+ 
+ 
+
+
 }
