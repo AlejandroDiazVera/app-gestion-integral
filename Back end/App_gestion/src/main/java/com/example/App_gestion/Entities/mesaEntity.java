fb79d9ee667c38deb4108388961a511623a0f456
JAlejandroDiaz <160150408+AlejandroDiazVera@users.noreply.github.com>
2024-03-02 07:11:10 -0500

    otras cosas

---
+++ Back end/App_gestion/src/main/java/com/example/App_gestion/Entities/mesaEntity.java
@@ -0,0 +1,24 @@
+package com.example.App_gestion.Entities;
+
+// import javax.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import lombok.*;
+
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @Getter
+// @Setter
+// @ToString
+// @Entity
+// @Table(name = "mesaentity")
+// public class mesaEntity implements Serializable {
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mesaentity_sequence")
+//     @SequenceGenerator(name="mesaentity_sequence", sequenceName="mesaentity_sequence", allocationSize=100)
+//     private Long id;
+    
+// }
--- Back end/App_gestion/src/main/java/com/example/App_gestion/Entities/negocio.java
+++ Back end/App_gestion/src/main/java/com/example/App_gestion/Entities/negocio.java
@@ -3,6 +3,7 @@ package com.example.App_gestion.Entities;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,10 @@ public class negocio implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "negocio_sequence")
     @SequenceGenerator(name = "negocio_sequence", sequenceName = "negocio_sequence", allocationSize = 100)
+    
     private Long id;
     private String nombre;
+    private String nit;
+    private String telefono;
 
 }
